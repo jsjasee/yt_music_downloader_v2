630470b6c7888e5318674b427e57a4d1fb7bb324
Jia Sheng Tay <tayjiasheng@Jias-MacBook-Pro.local>
2025-07-21 13:04:01 +0800

    fix song query bug

--- bot_manager.py
+++ bot_manager.py
@@ -39,6 +39,9 @@ class BotManager:
 
     # replacing all CHAT_ID with message.chat.id
     def search_music(self, message):
+        # if got any previous song lists, delete those to avoid user from clicking an old song list, causing errors
+        self.bot.delete_message(message.chat.id, self.user_last_message[message.chat.id])
+
         self.bot.send_message(chat_id=message.chat.id, text="🔎 Please enter the song name:")
         # change user state to 'searching song'
         self.user_state[message.chat.id] = "waiting_for_song"
